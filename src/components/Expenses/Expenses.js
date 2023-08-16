c0804f98905be6476a6192dae5dd6d39177b9964
Satish Panchal <satishdpanchal786@gmail.com>
2023-08-16 14:08:59 +0530

    implement conditional statement,filter futionality

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,11 +14,31 @@ const Expenses = (props) => {
     SetFilteredYear(selectedYear)
   }
 
+  const filteredExpenses = props.expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear
+  })
+
+  let expenseContent = <p>no expense found</p>;
+
+  if (filteredExpenses.length > 0) {
+    expenseContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   return (
     <div>
       <Card className="expenses">
-        <ExpensesFilter selected = {filteredYear} onChangeFilter = {filterChangeHandler} />
-        {props.expenses.map((expense) => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date ={expense.date}/>)}
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {expenseContent}
       </Card>
     </div>
   );
